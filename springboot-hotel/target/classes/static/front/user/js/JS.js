90a89833929a0676d33e3114729d91d1ebe7e519
Chema <48428337+kaysanshi@users.noreply.github.com>
2021-07-11 09:36:52 +0800

    Delete springboot-hotel/target directory

--- springboot-hotel/target/classes/static/front/user/js/JS.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-	$(".myMeituan").mouseover(function(){
-		$(".myMeituan_ul").css('display','block');
-	})
-	$(".myMeituan").mouseout(function(){
-		$(".myMeituan_ul").css('display','none');
-	})
-});
